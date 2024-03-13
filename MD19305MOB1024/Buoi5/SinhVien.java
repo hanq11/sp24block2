7c661dc7ff18bbce30a55904cef3a3a0fb08d798
Michael <>
2024-03-13 23:09:49 +0700

    update code buoi 5

---
+++ MD19305MOB1024/Buoi5/SinhVien.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MD19305MOB1024.Buoi5;
+
+/**
+ *
+ * @author syn
+ */
+public class SinhVien {
+    String ten;
+    int tuoi;
+
+    public SinhVien() {
+    }
+
+    public SinhVien(String ten, int tuoi) {
+        this.ten = ten;
+        this.tuoi = tuoi;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+    
+    
+}
